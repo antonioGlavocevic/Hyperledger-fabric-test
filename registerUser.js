1ea750e0e94b6907754a91ee5cdfd00b379437df
Antonio Glavocevic <tony@it-tony-mac.local>
2018-04-16 11:27:42 +1000

    Cleanup

--- registerUser.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var helper = require('./helper.js');
-
-helper.getRegisterUser('org1','user1');
-helper.getRegisterUser('org2','user2');
--- teardown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker rm -f $(docker ps -aq)
-
-docker rmi $(docker images | grep dev | awk '{print $3}')
-
-docker volume prune
-
-docker network prune
